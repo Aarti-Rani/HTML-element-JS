ea7355373d61db771f9a6923ed0c369a3cb27577
Aarti-Rani <aartiranicool@gmail.com>
2021-10-20 21:33:09 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 // Import stylesheets
-import './style.css';
-
-// Write Javascript code!
 console.clear();
 var list = document.getElementById("to-do");
 var btnAdd = document.getElementById("add-item");
-btnAdd.addEventListener("click", function () {
-	var newListElement=document.createElement('li');
-	var textNode=document.createTextNode('List Item'+(list.childElementCount+1));
-	newListElement.appendChild(textNode)
-	newListElement.id=("item"+(list.childElementCount+1))
-	list.appendChild(newListElement)
-	console.log(list.childElementCount);
+var btnUpdate = document.getElementById("update-item");
+var btnRemove = document.getElementById("remove-item");
+var inputBox = document.getElementById("todo-input");
+var currentInputValue = "";
+inputBox.addEventListener("input", function (e) {
+	currentInputValue = e.target.value;
+});
+inputBox.addEventListener("keyup", function (e) {
+	if (e.keyCode === 13) {
+		addListItem();
+	}
+});
+function createNewNode() {
+	var newListElement = document.createElement("li");
+	var textNode = document.createTextNode(currentInputValue);
+	//var textNode = document.createTextNode("List Item" + (list.childElementCount + 1)
+	newListElement.appendChild(textNode);
+	newListElement.id = "item" + (list.childElementCount + 1);
+	return newListElement;
+}
+//var firstItem=document.getElementById('item1')
+function addListItem() {
+	if (
+		currentInputValue !== undefined &&
+		currentInputValue !== null &&
+		currentInputValue !== ""
+	) {
+		var newListElement = createNewNode();
+		//var firstItem = list.firstElementChild;
+		//console.log(firstItem);
+		//list.insertBefore(newListElement, firstItem);
+		list.appendChild(newListElement);
+		console.log(list.childElementCount);
 		console.log(newListElement);
+		inputBox.value = "";
+		currentInputValue = "";
+	} else {
+		window.alert("Please enter a valid TODO");
+	}
+}
+btnAdd.addEventListener("click", addListItem);
+btnUpdate.addEventListener("click", function () {
+	var firstElement = list.firstElementChild;
+	var newListElement = createNewNode();
+	list.replaceChild(newListElement, firstElement);
+});
+btnRemove.addEventListener("click", function () {
+	var firstElement = list.firstElementChild;
+	list.removeChild(firstElement);
 });
